e283d90739fb21a2242e3d1240a8de459a03c2bb
Beshoi <beshoiemad16@gmail.com>
2022-03-30 17:23:35 +0200

    Export Components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,8 @@
 export { Meta } from './Meta/Meta';
-export { Header } from './Header/Header'
-export { Footer } from './Footer/Footer'
-export { PostItem } from './PostItem/PostItem'
+export { Header } from './Header/Header';
+export { Footer } from './Footer/Footer';
+export { PostItem } from './PostItem/PostItem';
+export { Hi } from './Hi/Hi';
+export { Card } from './Card/Card';
+export { ColorfulHeader } from './ColorfulHeader/ColorfulHeader'
+export { Box, Flex, Text } from './Utils/Utils';

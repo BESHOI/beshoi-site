45b0aaaee0e556f2d6a1283448b4380c8620c787
Beshoi <beshoiemad16@gmail.com>
2022-03-31 17:48:30 +0200

    Add More Gradients

--- src/components/ColorfulHeader/ColorfulHeader.tsx
+++ src/components/ColorfulHeader/ColorfulHeader.tsx
@@ -5,6 +5,7 @@ import { styled } from 'stitches.config';
 type Props = {
   header: string;
   emoji: string;
+  css?: object;
 };
 
 const Header = styled('h1', {
@@ -17,48 +18,67 @@ const Header = styled('h1', {
 
 const Emoji = styled('h2', {
   lineHeight: '1',
-  letterSpacing: '-25px',
 });
 
 const Colorful = () => {
-  const Names = [
+  const ColorsList = [
     '$indigo',
-    '$crimson',
+    '$grass',
     '$blue',
-    '$tomato',
     '$red',
-    '$violet',
     '$orange',
-    '$cyan',
+    '$violet',
     '$yellow',
-    '$lime',
     '$pink',
+    '$cyan',
+    '$green',
+    '$bronze',
+    '$plum',
+    '$teal',
+    '$sky',
+    '$lime',
+    '$crimson',
+    '$brown',
+    '$purple',
+    '$tomato',
+    '$amber',
+    '$gold',
     '$slate',
   ];
-  const RandomNumber = () => Math.floor(Math.random() * 7 + 4);
-  const Colors = Names.map((color) => color + RandomNumber());
-  const RandomColor = Colors[RandomNumber()];
-  return RandomColor;
+
+  const AnglesList = [45, 90, 135, 180, 225, 270, 315, 360];
+  const RandomAngle = Math.floor(Math.random() * (AnglesList.length - 1));
+  const Angle = AnglesList[RandomAngle] + 'deg';
+
+  const RandomNumber = (min: number, max: number) =>
+    Math.floor(Math.random() * (max - min + 1) + min);
+  const RandomValue = () => Math.floor(Math.random() * (ColorsList.length - 1));
+  const Colors = ColorsList.map((color) => color + RandomNumber(7, 12));
+  const RandomColor = Colors[RandomValue()];
+
+  return { RandomColor, Angle };
 };
 
-export const ColorfulHeader = ({ header, emoji }: Props) => {
+export const ColorfulHeader = ({ header, emoji, css }: Props) => {
   const [firstcolor, setFirstcolor] = useState('$blue5');
   const [secondColor, setSecondcolor] = useState('$crimson8');
+  const [angle, setAngle] = useState('45deg');
   useEffect(() => {
-    setFirstcolor(Colorful());
-    setSecondcolor(Colorful());
+    setFirstcolor(Colorful().RandomColor);
+    setSecondcolor(Colorful().RandomColor);
+    setAngle(Colorful().Angle);
   }, []);
 
   return (
     <Flex css={{ alignItems: 'center', gap: '$1' }}>
       <Header
         css={{
-          backgroundImage: `linear-gradient(45deg,${firstcolor}, ${secondColor} )`,
+          backgroundImage: `linear-gradient(${angle},${firstcolor}, ${secondColor} )`,
         }}
       >
         {header}
       </Header>
-      <Emoji>{emoji}</Emoji>
+      <Emoji css={{ ...css }}>{emoji}</Emoji>
     </Flex>
   );
 };

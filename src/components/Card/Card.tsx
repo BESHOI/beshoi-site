1d213e1b70816966b13b4aa33cbe9934f731a01a
Beshoi <beshoiemad16@gmail.com>
2022-04-04 15:07:29 +0200

    Update Cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Flex} from 'components';
+import { Flex } from 'components';
 import * as s from './Card.styled';
 
 type Props = {
@@ -7,23 +7,17 @@ type Props = {
   company?: string;
   header?: string;
   info: string;
-  color: string;
 };
 
-export const Card = ({ date, company, header, info, color }: Props) => {
+export const Card = ({ date, company, header, info }: Props) => {
   return (
-    <s.CardSection
-      css={{
-        backgroundColor: `${color}2`,
-        '@light': { backgroundColor: `${color}4` },
-      }}
-    >
+    <s.CardSection>
       <Flex css={{ gap: '$1' }}>
         {date && <s.CardSmall>{date}</s.CardSmall>}
         {company && <s.CardSmall>/ {company}</s.CardSmall>}
       </Flex>
-      <s.CardHeader>{header}</s.CardHeader>
-      <s.CardParagraph >{info}</s.CardParagraph>
+     <s.CardHeader>{header}</s.CardHeader>
+      <s.CardParagraph>{info}</s.CardParagraph>
     </s.CardSection>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,14 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Card, ColorfulHeader, Hi, PostItem } from 'components';
+import { Box, Card, ColorfulHeader, Hi, PostItem } from 'components';
 import getPosts from '../scripts/fileSystem';
 import { styled } from 'stitches.config';
-
-export const CardsGrid = styled('div', {
-  display: 'grid',
-  gridGap: '$7',
-  '@bp2': {
-    gridGap: '$3',
-  },
-});
+import { CardsGrid } from '../components/Card/Card.styled';
 
 export const HomeSection = styled('section', {
   display: 'grid',
   gridGap: '$3',
-  mt: '$7',
+  mt: '$4',
   '& h1': {
     mb: '$0',
   },
@@ -23,7 +16,9 @@ export const HomeSection = styled('section', {
 
 const Home: NextPage = ({ posts }: any) => {
   return (
-    <div>
+    <>
+      <HomeSection aria-labelledby="home">
+
       <Head>
         <title>Beshoi &#39;s Site</title>
         <meta name="description" content="My personal site" />
@@ -32,55 +27,49 @@ const Home: NextPage = ({ posts }: any) => {
 
       <Hi />
 
-      <HomeSection aria-labelledby="home">
-        <ColorfulHeader
-          header="About Me"
-          emoji="😎"
-        />
-        <Card
-          info="I am a Web Designer and Front End Developer based in Cairo, Egypt. I am working in the web / 
-          IT industry from last 4 years. Currently I am holding a position of Front End Developer in 
-          Blueprint. I have extensively worked on Designing and Coding various User Interfaces and 
-          Prototypes."
-          color="$indigo"
-        />
-
-        <ColorfulHeader
-          header="Experience"
-          emoji="🤵"
-        />
-        <CardsGrid>
-          <Card
-            date="January 18, 2020 - 2021"
-            company="Business Square"
-            header="Front End Developer"
-            info="Producing, maintaining and modifying websites and user interfaces. Ensuring websites are 
+        <Box css={{mt:'$4'}}>
+          <ColorfulHeader header="About Me" emoji="💻" />
+          <CardsGrid>
+            <Card
+              info="I am a Web Designer and Front End Developer based in Cairo, Egypt. I am working in the web / 
+            IT industry from last 4 years. Currently I am holding a position of Front End Developer in 
+            Blueprint. I have extensively worked on Designing and Coding various User Interfaces and 
+            Prototypes."
+            />
+          </CardsGrid>
+        </Box>
+        <Box>
+          <ColorfulHeader header="Experience" emoji="🤵" />
+          <CardsGrid>
+            <Card
+              date="January 18, 2020 - 2021"
+              company="Business Square"
+              header="Front End Developer"
+              info="Producing, maintaining and modifying websites and user interfaces. Ensuring websites are 
         accessible across many platforms, including laptops and smartphones. Fixing any website 
         issues or bugs that arise."
-            color="$indigo"
-          />
-          <Card
-            date="January 18, 2021 - now"
-            company="Blueprint"
-            header="Front End Developer"
-            info="Developing and maintaining the front-end functionality of websites, participating in 
+            />
+            <Card
+              date="January 18, 2021 - now"
+              company="Blueprint"
+              header="Front End Developer"
+              info="Developing and maintaining the front-end functionality of websites, participating in 
         discussions with clients to clarify what they want, developing websites that have a consistent 
         feel and look throughout all web properties, providing technical support to end users."
-            color="$crimson"
-          />
-        </CardsGrid>
+            />
+          </CardsGrid>
+        </Box>
 
-        <ColorfulHeader
-          header="Latest Posts"
-          emoji="📚"
-        />
-        <CardsGrid>
-          {posts.map((post: any) => (
-            <PostItem key={post.slug} post={post} />
-          ))}
-        </CardsGrid>
+        <Box>
+          <ColorfulHeader header="Latest Posts" emoji="📚" />
+          <CardsGrid>
+            {posts.map((post: any) => (
+              <PostItem key={post.slug} post={post} />
+            ))}
+          </CardsGrid>
+        </Box>
       </HomeSection>
-    </div>
+    </>
   );
 };
 

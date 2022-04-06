fcb6b15a92912a4488259a4e93806975fb26f980
Beshoi <beshoiemad16@gmail.com>
2022-04-06 14:12:24 +0200

    loop on card data insted of pass props

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,23 +1,66 @@
 import React from 'react';
 import { Flex } from 'components';
 import * as s from './Card.styled';
+import { about, experience, contact } from './CardData';
 
 type Props = {
-  date?: string;
-  company?: string;
-  header?: string;
-  info: string;
+  data: string;
 };
 
-export const Card = ({ date, company, header, info }: Props) => {
+export const Card = ({ data }: Props) => {
   return (
-    <s.CardSection>
-      <Flex css={{ gap: '$1' }}>
-        {date && <s.CardSmall>{date}</s.CardSmall>}
-        {company && <s.CardSmall>/ {company}</s.CardSmall>}
-      </Flex>
-     <s.CardHeader>{header}</s.CardHeader>
-      <s.CardParagraph>{info}</s.CardParagraph>
-    </s.CardSection>
+    <>
+      {data === 'about' && (
+        <s.CardsGrid>
+          {about.map((item, index) => (
+            <s.CardSection key={index}>
+              <s.CardParagraph>{item.about}</s.CardParagraph>
+            </s.CardSection>
+          ))}
+        </s.CardsGrid>
+      )}
+
+      {data === 'experience' && (
+        <s.CardsGrid>
+          {experience.map((item) => (
+            <s.CardSection key={item.date}>
+              <Flex css={{ gap: '$1' }}>
+                <s.CardSmall>{item.date}</s.CardSmall>
+                <s.CardSmall>/ {item.company}</s.CardSmall>
+              </Flex>
+              <s.CardHeader>{item.header}</s.CardHeader>
+              <s.CardParagraph>{item.info}</s.CardParagraph>
+            </s.CardSection>
+          ))}
+        </s.CardsGrid>
+      )}
+
+      {data === 'contact' && (
+        <s.CardsGrid css={{ gridGap: '$3' }}>
+          {contact.map((item) => (
+            <s.CardSection
+              key={item.text}
+              css={{
+                display: 'grid',
+                gap: '$1',
+                gridTemplateColumns: '35px 1fr',
+                alignItems: 'center',
+                '& p': {
+                  mt: '$0',
+                },
+                '&> *': {
+                  fontSize: '$3',
+                },
+              }}
+            >
+              <s.CardParagraph>
+                <s.CardIcon icon={item.icon} />
+              </s.CardParagraph>
+              <a href={item.link}>{item.text}</a>
+            </s.CardSection>
+          ))}
+        </s.CardsGrid>
+      )}
+    </>
   );
 };
---
+++ src/components/Card/CardData.ts
@@ -0,0 +1,44 @@
+export const about = [
+  {
+    about:
+      'I am a Web Designer and Front End Developer based in Cairo, Egypt. I am working in the web /  IT industry from last 4 years. Currently I am holding a position of Front End Developer in Blueprint. I have extensively worked on Designing and Coding various User Interfaces and Prototypes.',
+  },
+];
+
+export const experience = [
+  {
+    date: 'January 18, 2020 - 2021',
+    company: 'Business Square',
+    header: 'Front End Developer',
+    info: 'Producing, maintaining and modifying websites and user interfaces. Ensuring websites are accessible across many platforms, including laptops and smartphones. Fixing any website issues or bugs that arise.',
+  },
+  {
+    date: 'January 18, 2021 - now',
+    company: 'Blueprint',
+    header: 'Front End Developer',
+    info: 'Developing and maintaining the front-end functionality of websites, participating in discussions with clients to clarify what they want, developing websites that have a consistent feel and look throughout all web properties, providing technical support to end users.',
+  },
+];
+
+export const contact = [
+  {
+    text: 'Email/beshoi16@gmail.com',
+    icon: 'fontisto:email',
+    link: 'mailto:beshoiemad16@gmail.com',
+  },
+  {
+    text: 'Github/beshoi',
+    icon: 'simple-icons:github',
+    link: '//github.com/Beshoi',
+  },
+  {
+    text: 'Linkedin/beshoi-emad',
+    icon: 'simple-icons:linkedin',
+    link: '//linkedin.com/in/beshoi-emad',
+  },
+  {
+    text: 'Twitter/beshoi-emad',
+    icon: 'simple-icons:twitter',
+    link: '//twitter.com/beshoi_emad',
+  },
+];

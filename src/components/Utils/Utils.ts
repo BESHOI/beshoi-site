c8cdd0d49f7d937281057f236abc78c9eaed2be7
Beshoi <beshoiemad16@gmail.com>
2022-04-06 14:10:41 +0200

    add grid

--- src/components/Utils/Utils.ts
+++ src/components/Utils/Utils.ts
@@ -4,5 +4,5 @@ type Props = {};
 
 export const Box = styled('div', {});
 export const Flex = styled('div', { display: 'flex' });
-
+export const Grid = styled('div', { display: 'grid' });
 export const Text = styled('p', {});

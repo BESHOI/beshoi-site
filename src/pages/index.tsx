6e37b962bf2ee18aa0b83d382f05fdc6bf1ca702
Beshoi <beshoiemad16@gmail.com>
2022-04-07 09:29:46 +0200

    order posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Box, Card, ColorfulHeader, Hi, PostItem } from 'components';
+import { Card, ColorfulHeader, Hi, PostItem } from 'components';
 import getPosts from '../scripts/fileSystem';
 import { styled } from 'stitches.config';
 import { CardsGrid } from '../components/Card/Card.styled';
 
-export const HomeSection = styled('section', {
+const HomeSection = styled('section', {
   display: 'grid',
   gridGap: '$3',
   mt: '$4',
@@ -14,56 +14,45 @@ export const HomeSection = styled('section', {
   },
 });
 
+const Box = styled('div', {
+  mt: '$4',
+});
+
 const Home: NextPage = ({ posts }: any) => {
+  const Posts = posts.slice().map((obj: any) => {
+    return { ...obj, date: new Date(obj.data.date) };
+  });
+
+  const sortedPosts = Posts.sort(
+    (a: { date: string }, b: { date: string }) =>
+      new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
+
   return (
     <>
       <HomeSection aria-labelledby="home">
+        <Head>
+          <title>Beshoi &#39;s Site</title>
+          <meta name="description" content="My personal site" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <Head>
-        <title>Beshoi &#39;s Site</title>
-        <meta name="description" content="My personal site" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+        <Hi />
 
-      <Hi />
-
-        <Box css={{mt:'$4'}}>
+        <Box>
           <ColorfulHeader header="About Me" emoji="💻" />
-          <CardsGrid>
-            <Card
-              info="I am a Web Designer and Front End Developer based in Cairo, Egypt. I am working in the web / 
-            IT industry from last 4 years. Currently I am holding a position of Front End Developer in 
-            Blueprint. I have extensively worked on Designing and Coding various User Interfaces and 
-            Prototypes."
-            />
-          </CardsGrid>
+          <Card data="about" />
         </Box>
+
         <Box>
           <ColorfulHeader header="Experience" emoji="🤵" />
-          <CardsGrid>
-            <Card
-              date="January 18, 2020 - 2021"
-              company="Business Square"
-              header="Front End Developer"
-              info="Producing, maintaining and modifying websites and user interfaces. Ensuring websites are 
-        accessible across many platforms, including laptops and smartphones. Fixing any website 
-        issues or bugs that arise."
-            />
-            <Card
-              date="January 18, 2021 - now"
-              company="Blueprint"
-              header="Front End Developer"
-              info="Developing and maintaining the front-end functionality of websites, participating in 
-        discussions with clients to clarify what they want, developing websites that have a consistent 
-        feel and look throughout all web properties, providing technical support to end users."
-            />
-          </CardsGrid>
+          <Card data="experience" />
         </Box>
 
         <Box>
           <ColorfulHeader header="Latest Posts" emoji="📚" />
           <CardsGrid>
-            {posts.map((post: any) => (
+            {sortedPosts.map((post: any) => (
               <PostItem key={post.slug} post={post} />
             ))}
           </CardsGrid>

e8a996c1f9f17a331f9691d7b8d3d8e7c06ac258
Beshoi <beshoiemad16@gmail.com>
2022-04-07 09:29:46 +0200

    order posts

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -4,16 +4,25 @@ import { PostItem, Meta, ColorfulHeader } from 'components';
 import { CardsGrid } from '../components/Card/Card.styled';
 
 type Props = {
-  posts: any;
+  posts: [];
 };
 
 const Blog = ({ posts }: Props) => {
+  const Posts = posts.slice().map((obj: any) => {
+    return { ...obj, date: new Date(obj.data.date) };
+  });
+
+  const sortedPosts = Posts.sort(
+    (a: { date: string }, b: { date: string }) =>
+      new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
+
   return (
     <>
-      <Meta title="Blog"/>
+      <Meta title="Blog" />
       <ColorfulHeader header="All Posts" emoji="📚" />
       <CardsGrid>
-        {posts.map((post: any) => (
+        {sortedPosts.map((post) => (
           <PostItem key={post.slug} post={post} />
         ))}
       </CardsGrid>

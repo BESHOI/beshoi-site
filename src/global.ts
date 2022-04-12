1e71af9c8c129d5af1a1fabc7775fbefcb5e7abe
Beshoi <beshoiemad16@gmail.com>
2022-04-12 12:45:38 +0200

    add font cache

--- src/global.ts
+++ src/global.ts
@@ -1,13 +1,15 @@
 import '../node_modules/modern-normalize/modern-normalize.css';
-import '@fontsource/inter/variable.css';
 import { globalCss, darkTheme } from './stitches.config';
 
 export const globalStyles = globalCss({
   '@font-face': {
-    fontFamily: 'Inter-fallback',
-    src: "local('Arial')",
-    fontWeight: 'normal',
+    fontFamily: 'InterVariable',
+    fontStyle: 'normal',
     fontDisplay: 'optional',
+    fontWeight: '100 900',
+    src: `url('/font/inter-latin-variable-wghtOnly-normal.woff2') format('woff2') `,
+    unicodeRange:
+      'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD',
   },
 
   // '*,*::before,*::after': { margin: '0', padding: 0 },
@@ -18,7 +20,16 @@ export const globalStyles = globalCss({
 
   body: {
     minBlockSize: '100%',
-    fontFamily: `'interVariable', 'Inter-fallback'`,
+    fontFamily: `'interVariable',
+    system-ui,
+		-apple-system,
+		'Segoe UI',
+		Roboto,
+		Helvetica,
+		Arial,
+		sans-serif,
+		'Apple Color Emoji',
+		'Segoe UI Emoji'`,
     textRendering: 'optimizeSpeed',
     fontWeight: '$2',
     lineHeight: '$3',

ee764b1eaee28df705dc9e59c4395b0cce511b78
Beshoi <beshoiemad16@gmail.com>
2022-05-16 09:48:13 +0200

    preload font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,15 @@ class Document extends NextDocument<Props> {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            rel="preload"
+            href="/font/inter-latin-variable-wghtOnly-normal.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />

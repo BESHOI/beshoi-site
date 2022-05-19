a03f37be4bcad1513e37f0714aceda0bb8ee0b76
Beshoi <beshoiemad16@gmail.com>
2022-05-19 16:14:17 +0200

    update icons & colors

--- src/components/Tools/ToolsData.ts
+++ src/components/Tools/ToolsData.ts
@@ -2,39 +2,28 @@ export const ToolsData = [
   // Browsing
   {
     title: 'Browsing',
-    color: 'var(--colors-red9)',
     list: [
       {
         name: 'Firefox',
-        icon: 'simple-icons:firefox',
         link: '#',
       },
 
       {
         name: 'Google Chrome',
-        icon: 'simple-icons:googlechrome',
         link: '#',
       },
       {
         name: 'Ublockorigin',
-        icon: 'simple-icons:ublockorigin',
         link: '#',
       },
 
       {
         name: 'Bitwarden',
-        icon: 'simple-icons:bitwarden',
-        link: '#',
-      },
-      {
-        name: 'Dark Reader',
-        icon: 'simple-icons:darkreader',
         link: '#',
       },
 
       {
         name: "I don't care cookies",
-        icon: 'fluent:cookies-20-filled',
         link: '#',
       },
     ],
@@ -43,23 +32,19 @@ export const ToolsData = [
   // Development
   {
     title: 'Development',
-    color: 'var(--colors-cyan9)',
     list: [
       {
         name: 'Visual Studio Code',
-        icon: 'simple-icons:visualstudiocode',
         link: '#',
       },
 
       {
         name: 'Atom one Dark Theme',
-        icon: 'clarity:plugin-solid',
         link: 'https://marketplace.visualstudio.com/items?itemName=akamud.vscode-theme-onedark',
       },
 
       {
         name: 'Ariake Dark',
-        icon: 'clarity:plugin-solid',
         link: 'https://marketplace.visualstudio.com/items?itemName=wart.ariake-dark',
       },
     ],
@@ -68,37 +53,30 @@ export const ToolsData = [
   // Images
   {
     title: 'Images',
-    color: 'var(--colors-crimson9)',
     list: [
+      {
+        name: 'Squoosh (compress images)',
+        link: 'https://squoosh.app/',
+      },
 
       {
         name: 'loremflickr (Generate dummy Images)',
-        icon: 'bx:link',
         link: 'https://loremflickr.com/',
       },
     ],
   },
 
-// Fonts
+  // Fonts
   {
     title: 'Fonts',
-    color: 'var(--colors-crimson9)',
     list: [
       {
-        name: 'Squoosh (compress images)',
-        icon: 'bx:link',
-        link: 'https://squoosh.app/',
-      },
-
-      {
         name: 'FontSources (Self-host Open Source fonts in neatly bundled NPM packages)',
-        icon: 'bx:link',
         link: 'https://fontsource.org/',
       },
 
       {
         name: 'loremflickr (Generate dummy Images)',
-        icon: 'bx:link',
         link: 'https://loremflickr.com/',
       },
     ],

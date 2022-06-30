4e30b4a0e43d81efa6efe345e994bc0be04f4f4f
Beshoi <beshoiemad16@gmail.com>
2022-06-30 17:45:14 +0200

    add search box

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import getPosts from 'scripts/fileSystem';
 import { PostItem, Meta, ColorfulHeader } from 'components';
 import { CardsGrid } from 'components/Card/Card.styled';
+import { styled } from 'stitches.config';
 
 export type SortedPosts = {
   SortedPosts: [
@@ -19,19 +20,44 @@ export type SortedPosts = {
   ];
 };
 
-const Blog = ({ SortedPosts }: SortedPosts) => {
-  const BlogPosts = SortedPosts.filter((post) => post.data.tag !== 'tools');
+const SearchInput = styled('input', {
+  display: 'inline-block',
+  padding: '$2',
+  width: '100%',
+  margin: '$2 0',
+  backgroundColor: '$background',
+  border: '2px solid $slate9',
+  borderRadius: '$2',
+  outline: 'none',
+});
 
+const Blog = ({ SortedPosts }: SortedPosts) => {
+  const [searchkeyowrd, setSearchKeyowrd] = useState('');
+  const keyword = searchkeyowrd.toLocaleLowerCase();
+  // const BlogPosts = SortedPosts.filter((post) => post.data.tag !== 'tools');
   return (
-    <>
-      <Meta title="Blog" />
-      <ColorfulHeader header="All Posts" emoji="📚" />
+    <section aria-labelledby="blog">
+      <Meta title="Blog - Beshoi Emad" description="My blog" />
+      <ColorfulHeader header="All Posts" emoji="📚" id="blog" />
+
+      <SearchInput
+        type="text"
+        placeholder="Search"
+        onChange={(e) => setSearchKeyowrd(e.currentTarget.value)}
+        value={searchkeyowrd}
+      />
+
       <CardsGrid>
-        {BlogPosts.map((post) => (
-          <PostItem key={post.slug} post={post} />
-        ))}
+        {searchkeyowrd === ''
+          ? SortedPosts.map((post) => <PostItem key={post.slug} post={post} />)
+          : SortedPosts.filter((post) =>
+              post.data.title.toLowerCase().includes(keyword) ||
+              post.data.excerpt.toLowerCase().includes(keyword)
+                ? post
+                : null
+            )?.map((post) => <PostItem key={post.slug} post={post} />)}
       </CardsGrid>
-    </>
+    </section>
   );
 };
 

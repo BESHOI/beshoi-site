110c467ecd693798f83b641cac40d53f09cb9f02
Beshoi <beshoiemad16@gmail.com>
2022-06-30 17:31:56 +0200

    add next-seo

--- src/components/Meta/Meta.tsx
+++ src/components/Meta/Meta.tsx
@@ -1,22 +1,62 @@
 import React from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { NextSeo } from 'next-seo';
 
 type Props = {
   title: string;
-  content: string;
+  description: string;
 };
 
-export const Meta = ({ title, content }: Props) => {
+export const Meta = ({ title, description }: Props) => {
+  const router = useRouter();
+
   return (
-    <Head>
-      <title>{title}</title>
-      <meta name="keywords" content={content} />
-    </Head>
+    <>
+      <Head>
+        <meta charSet="UTF-8" key="charset" />
+        <meta name="keywords" content={MetaInfo.content} />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1"
+          key="viewport"
+        />
+      </Head>
+
+      <NextSeo
+        title={title ? title : MetaInfo.title}
+        description={description ? description : MetaInfo.content}
+        canonical={`https://beshoi.dev${router.asPath}/`}
+        openGraph={{
+          title: title ? title : MetaInfo.title,
+          description: description ? description : MetaInfo.content,
+          url: `https://beshoi.dev${router.asPath}/`,
+          images: [
+            {
+              url: 'https://www.beshoi.dev/imgs/beshoi.jpg',
+              width: 400,
+              height: 400,
+              alt: 'Og Image Alt',
+              type: 'image/jpeg',
+            },
+          ],
+          locale: MetaInfo.locale,
+          site_name: MetaInfo.site_name,
+        }}
+        twitter={{
+          handle: '@beshoi_emad',
+          site: '@beshoi_emad',
+          cardType: 'summary_large_image',
+        }}
+      />
+    </>
   );
 };
 
-Meta.defaultProps = {
-  title: 'BeshoiBlog - Your one stop blog for everything React',
+const MetaInfo = {
+  site_name: "Beshoi's site",
+  title: 'Beshoi Emad - Front End Developer',
   content:
-    'html, css, react, tools, blog, Beshoi Emad, tutorial, tips, tricks, tips && tricks',
+    'html, css, flex, grid, js, react, blog, tools, extensions, terminal, commandline, bash, zsh, powershell, linux, arch, i3wm, package manager, vim, neovim, tutorials, tips, tricks, Beshoi Emad',
+  locale: 'en',
 };

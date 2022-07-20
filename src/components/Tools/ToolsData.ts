f6c52528620e8c8095d2cb67bb8abec4e99caed7
Beshoi <beshoiemad16@gmail.com>
2022-07-20 16:13:47 +0200

    update tools

--- src/components/Tools/ToolsData.ts
+++ src/components/Tools/ToolsData.ts
@@ -15,6 +15,62 @@ export const ToolsData = [
     ],
   },
 
+  // Terminal Tools
+  {
+    title: 'Terminal Tools',
+    list: [
+      {
+        name: 'Gh',
+        link: 'https://cli.github.com/',
+      },
+
+      {
+        name: 'Yt-dlp',
+        link: 'https://github.com/yt-dlp/yt-dlp',
+      },
+
+      {
+        name: 'Mpv',
+        link: 'https://github.com/mpv-player/mpv',
+      },
+
+      {
+        name: 'Fzf',
+        link: 'https://github.com/junegunn/fzf',
+      },
+
+      {
+        name: 'Ytfzf',
+        link: 'https://github.com/pystardust/ytfzf',
+      },
+
+      {
+        name: 'Neovim',
+        link: 'https://github.com/neovim/neovim',
+      },
+
+      {
+        name: 'Squoosh',
+        link: 'https://github.com/GoogleChromeLabs/squoosh/tree/dev/cli',
+      },
+
+      {
+        name: 'Wttr.in',
+        link: 'https://github.com/chubin/wttr.in',
+      },
+
+      {
+        name: 'Buku',
+        link: 'https://github.com/jarun/buku',
+      },
+
+      {
+        name: 'Qalculate',
+        link: 'https://qalculate.github.io/',
+      },
+    ],
+  },
+
   // Browsing
   {
     title: 'Browsing',
@@ -28,6 +84,12 @@ export const ToolsData = [
         name: 'Google Chrome',
         link: '#',
       },
+
+      {
+        name: 'Ungoogled Chromium',
+        link: 'https://github.com/ungoogled-software/ungoogled-chromium',
+      },
+
       {
         name: 'Ublockorigin',
         link: '#',
@@ -63,6 +125,51 @@ export const ToolsData = [
         name: 'Ariake Dark',
         link: 'https://marketplace.visualstudio.com/items?itemName=wart.ariake-dark',
       },
+
+      {
+        name: 'Deno',
+        link: 'https://marketplace.visualstudio.com/items?itemName=denoland.vscode-deno',
+      },
+
+      {
+        name: 'Path Intellisense',
+        link: 'https://marketplace.visualstudio.com/items?itemName=christian-kohler.path-intellisense',
+      },
+
+      {
+        name: 'Prettier',
+        link: 'https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode',
+      },
+
+      {
+        name: 'Sass Intellisense',
+        link: 'https://marketplace.visualstudio.com/items?itemName=mrmlnc.vscode-scss',
+      },
+
+      {
+        name: 'Auto Rename Tag',
+        link: 'https://marketplace.visualstudio.com/items?itemName=formulahendry.auto-rename-tag',
+      },
+
+      {
+        name: 'Color Highlight',
+        link: 'https://marketplace.visualstudio.com/items?itemName=naumovs.color-highlight',
+      },
+
+      {
+        name: 'Css Var Complete',
+        link: 'https://marketplace.visualstudio.com/items?itemName=phoenisx.cssvar',
+      },
+
+      {
+        name: 'Markdown All in One',
+        link: 'https://marketplace.visualstudio.com/items?itemName=yzhang.markdown-all-in-one',
+      },
+
+      {
+        name: 'Vscode MDX',
+        link: 'https://marketplace.visualstudio.com/items?itemName=JounQin.vscode-mdx',
+      },
     ],
   },
 
@@ -90,10 +197,21 @@ export const ToolsData = [
         name: 'FontSources (Self-host Open Source fonts in neatly bundled NPM packages)',
         link: 'https://fontsource.org/',
       },
+    ],
+  },
+
+  // Templates
+  {
+    title: 'Templates',
+    list: [
+      {
+        name: 'Typescript Nextjs Starter',
+        link: 'https://github.com/jpedroschmitz/typescript-nextjs-starter',
+      },
 
       {
-        name: 'loremflickr (Generate dummy Images)',
-        link: 'https://loremflickr.com/',
+        name: 'Create T3 App',
+        link: 'https://github.com/t3-oss/create-t3-app',
       },
     ],
   },
